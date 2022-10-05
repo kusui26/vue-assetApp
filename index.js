a66f20edeee56e13ba79665e9501ae8a9fd23b5a
kusui26 <kensuke26grampus@gmai.com>
2022-10-05 09:19:39 +0900

    Heroku対応

--- index.js
+++ index.js
@@ -91,4 +91,4 @@ app.post('/exchange/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
